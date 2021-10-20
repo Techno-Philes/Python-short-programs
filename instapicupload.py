6c197bc6c2dc5919adaa78423d592342bd08c766
Karthik Mothiki <62557178+KarthikMothiki@users.noreply.github.com>
2021-10-20 12:50:13 +0530

    Delete instapicupload.py

--- instapicupload.py
+++
@@ -1,6 +0,0 @@
-from instabot import Bot
-
-bot = Bot()
-
-bot.login(username="enter_username",password="enter_password")
-bot.upload_photo("abc.jpg",caption="enter_any_caption")                                              
